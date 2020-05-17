d341ad7afdfdde79669e6cff115accf2e1747226
Sovandara UOM <uom.sovanndara@gmail.com>
2020-05-17 22:06:54 +0700

    Add passport library & production controller

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', 'API\RegisterController@register');
+Route::post('login', 'API\RegisterController@login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group( function () {
+    Route::resource('products', 'API\ProductController');
 });
